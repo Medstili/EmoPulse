bd95625c6e8f6a92967f9653fd0888bd9aa7b48e
mohamed_stili <codehub2023@gmail.com>
2025-07-07 17:11:04 +0100

    handling strings translation and deleting en string.xml

--- app/src/main/java/com/medstili/emopulse/activities/splashActivity.java
+++ app/src/main/java/com/medstili/emopulse/activities/splashActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
